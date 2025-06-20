85543c837c1c65f97555c147eb9294a81a641daf
Rishi <93063896+rishi-commits-bugs@users.noreply.github.com>
2025-06-20 17:36:45 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# Script to calculate Simple Interest
+
+echo "Enter Principal:"
+read p
+echo "Enter Rate of interest:"
+read r
+echo "Enter Time (in years):"
+read t
+
+si=$(echo "scale=2; $p * $r * $t / 100" | bc)
+echo "Simple Interest: $si"
 
